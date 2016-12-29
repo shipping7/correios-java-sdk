255f8694438082eb664be938fcb9be39de875a9f
Alexandre Gama <alexandre.gama.lima@gmail.com>
2016-12-29 14:49:12 -0200

    Adiciona stubs novamente de Producao

---
+++ src/main/java/br/com/correios/webservice/postagem/Pessoa.java
@@ -0,0 +1,335 @@
+
+package br.com.correios.webservice.postagem;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlSeeAlso;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for pessoa complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="pessoa">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="bairro" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="cep" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="cidade" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="complemento" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="ddd" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="endereco" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="nome" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="numero" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="pais" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="telefone" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="uf" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "pessoa", propOrder = {
+    "bairro",
+    "cep",
+    "cidade",
+    "complemento",
+    "ddd",
+    "endereco",
+    "nome",
+    "numero",
+    "pais",
+    "telefone",
+    "uf"
+})
+@XmlSeeAlso({
+    Destinatario.class,
+    Remetente.class
+})
+public class Pessoa {
+
+    protected String bairro;
+    protected String cep;
+    protected String cidade;
+    protected String complemento;
+    protected String ddd;
+    protected String endereco;
+    protected String nome;
+    protected String numero;
+    protected String pais;
+    protected String telefone;
+    protected String uf;
+
+    /**
+     * Gets the value of the bairro property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getBairro() {
+        return bairro;
+    }
+
+    /**
+     * Sets the value of the bairro property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setBairro(String value) {
+        this.bairro = value;
+    }
+
+    /**
+     * Gets the value of the cep property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCep() {
+        return cep;
+    }
+
+    /**
+     * Sets the value of the cep property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCep(String value) {
+        this.cep = value;
+    }
+
+    /**
+     * Gets the value of the cidade property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCidade() {
+        return cidade;
+    }
+
+    /**
+     * Sets the value of the cidade property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCidade(String value) {
+        this.cidade = value;
+    }
+
+    /**
+     * Gets the value of the complemento property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getComplemento() {
+        return complemento;
+    }
+
+    /**
+     * Sets the value of the complemento property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setComplemento(String value) {
+        this.complemento = value;
+    }
+
+    /**
+     * Gets the value of the ddd property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getDdd() {
+        return ddd;
+    }
+
+    /**
+     * Sets the value of the ddd property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDdd(String value) {
+        this.ddd = value;
+    }
+
+    /**
+     * Gets the value of the endereco property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getEndereco() {
+        return endereco;
+    }
+
+    /**
+     * Sets the value of the endereco property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setEndereco(String value) {
+        this.endereco = value;
+    }
+
+    /**
+     * Gets the value of the nome property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * Sets the value of the nome property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setNome(String value) {
+        this.nome = value;
+    }
+
+    /**
+     * Gets the value of the numero property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getNumero() {
+        return numero;
+    }
+
+    /**
+     * Sets the value of the numero property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setNumero(String value) {
+        this.numero = value;
+    }
+
+    /**
+     * Gets the value of the pais property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getPais() {
+        return pais;
+    }
+
+    /**
+     * Sets the value of the pais property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setPais(String value) {
+        this.pais = value;
+    }
+
+    /**
+     * Gets the value of the telefone property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * Sets the value of the telefone property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setTelefone(String value) {
+        this.telefone = value;
+    }
+
+    /**
+     * Gets the value of the uf property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getUf() {
+        return uf;
+    }
+
+    /**
+     * Sets the value of the uf property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setUf(String value) {
+        this.uf = value;
+    }
+
+}
---
+++ src/main/java/br/com/correios/webservice/postagem/StatusObjetoPostal.java
@@ -0,0 +1,57 @@
+
+package br.com.correios.webservice.postagem;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for statusObjetoPostal.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="statusObjetoPostal">
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *     &lt;enumeration value="EmBranco"/>
+ *     &lt;enumeration value="Postado"/>
+ *     &lt;enumeration value="Cancelado"/>
+ *     &lt;enumeration value="Estorno"/>
+ *   &lt;/restriction>
+ * &lt;/simpleType>
+ * </pre>
+ * 
+ */
+@XmlType(name = "statusObjetoPostal")
+@XmlEnum
+public enum StatusObjetoPostal {
+
+    @XmlEnumValue("EmBranco")
+    EM_BRANCO("EmBranco"),
+    @XmlEnumValue("Postado")
+    POSTADO("Postado"),
+    @XmlEnumValue("Cancelado")
+    CANCELADO("Cancelado"),
+    @XmlEnumValue("Estorno")
+    ESTORNO("Estorno");
+    private final String value;
+
+    StatusObjetoPostal(String v) {
+        value = v;
+    }
+
+    public String value() {
+        return value;
+    }
+
+    public static StatusObjetoPostal fromValue(String v) {
+        for (StatusObjetoPostal c: StatusObjetoPostal.values()) {
+            if (c.value.equals(v)) {
+                return c;
+            }
+        }
+        throw new IllegalArgumentException(v);
+    }
+
+}
