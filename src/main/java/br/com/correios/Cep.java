94f577b2e7b377cc16e119aebdaca83a1e755a95
Douglas Campos <login+douglascampos@elo7.com>
2017-06-07 11:44:20 -0300

    cria regra para exigir cep formatado

--- src/main/java/br/com/correios/Cep.java
+++ src/main/java/br/com/correios/Cep.java
@@ -1,10 +1,16 @@
 package br.com.correios;
 
+import java.util.regex.Pattern;
+
 public class Cep {
 
 	private final String value;
 
 	public Cep(String value) {
+		if (!Pattern.matches("\\d{8}", value)) {
+			throw new IllegalArgumentException("O cep deve estar formatado somente com 8 numeros");
+		}
+
 		this.value = value;
 	}
 
