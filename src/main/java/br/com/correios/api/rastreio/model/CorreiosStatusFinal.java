3cce957b8f8ac9650c6f00527f87a26236b80635
Eric Dallo <login+eric.dallo@elo7.com>
2017-12-22 11:26:13 -0200

    Remove alguns dos status final

--- src/main/java/br/com/correios/api/rastreio/model/CorreiosStatusFinal.java
+++ src/main/java/br/com/correios/api/rastreio/model/CorreiosStatusFinal.java
@@ -1,9 +1,10 @@
 package br.com.correios.api.rastreio.model;
 
 enum CorreiosStatusFinal {
-	BDE0, BDE1, BDE9, BDE28, BDE37, BDE43, BDE50, BDE51, BDE52,
-	BDI0, BDI1, BDI9, BDI28, BDI37, BDI43, BDI50, BDI51, BDI52,
-	BDR0, BDR1, BDR9, BDR28, BDR37, BDR43, BDR50, BDR51, BDR52,
+	BDE0, BDE1, BDE24,
+	BDI0, BDI1, BDI24,
+	BDR0, BDR1, BDR24,
+	LDI1, LDI2, LDI3, LDI4, LDI14,
 	;
 
 	public static boolean isFinal(String tipo, String status) {
